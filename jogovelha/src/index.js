1ae32e25741edb420aefa1ccd95c7753a1017203
Lincoln-Silva <lincolnalexa@gmail.com>
2023-07-13 23:27:23 -0300

    Mostrando as jogadas

--- jogovelha/src/index.js
+++ jogovelha/src/index.js
@@ -69,18 +69,27 @@ class Game extends React.Component {
     if(winner) {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-    
+    const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return(
+            <li>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
     return (
       <div className="game">
         <div className="game-board">
           <Board 
           squares={current.squares}
-          onClick={(i) => this.handeClick(i)} 
+          onClick={(i) => this.handleClick(i)} 
           />
         </div>
         <div className="game-info">
-          <div>{status} </div>
-          <ol>{/* TODO */} </ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
